8f980f98207b96dd4e5524f0ce99cb3a8264f683
Zakariya Sattar <zakariyasattar03@gmail.com>
2023-08-08 18:36:33 -0600

    front end vendor page

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -1,7 +1,7 @@
 $(document).ready(
     function()
     {
-      document.getElementById("vendor-name").textContent = "Catering Company #1" + "!";
+      document.getElementById("vendor-name").textContent = "Example Catering" + "!";
       document.getElementById("rating").textContent = "4.4";
       document.getElementById("num-of-reviews").textContent = "1381";
 
